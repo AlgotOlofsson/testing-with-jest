2731eb9d35925877eec4c746726833c34fdc4121
AlgotOlofsson <algot@omail.se>
2025-05-13 11:56:40 +0200

    Lade till mina tester och de misslyckades

---
+++ tests/browser.selen.test.js
@@ -0,0 +1,23 @@
+const { Builder, By } = require('selenium-webdriver');
+require('geckodriver');
+
+const fileUnderTest = 'file://' + __dirname.replaceAll(/ /g, '%20').replaceAll(/\\/g, '/') + '/../dist/index.html';
+const defaultTimeout = 10000;
+let driver;
+jest.setTimeout(1000 * 60 * 5); // 5 minuter
+
+beforeAll(async () => {
+console.log(fileUnderTest);
+    driver = await new Builder().forBrowser('firefox').build();
+    await driver.get(fileUnderTest);
+});
+
+afterAll(async () => {
+    await driver.quit();
+}, defaultTimeout);
+
+test('The push button should have correct label text', async () => {
+    const pushButton = await driver.findElement(By.name('push'));
+    const label = await pushButton.getText();
+    expect(label).toContain('Fel text')
+}) 
---
+++ tests/stack.jest.test.js
@@ -0,0 +1,8 @@
+const stack = require('../src/stack');
+
+test('peek returns same value twice if stack is unchanged', () =>{
+    stack.push('bananfluga');
+    const one = stack.peek();
+    const two = stack.peek();
+    expect(one).toBe('något fel');
+});
